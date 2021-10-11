56214e3e3840e5e587bd9b0e118c5cdf4b42befa
shaun276 <helloshaunak276@gmail.com>
2021-10-11 16:15:55 +0530

    info

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import telebot
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 from dotenv import load_dotenv
 
+
 import time
 import os
 import requests
@@ -13,11 +14,37 @@ WEATHER_API_KEY=os.getenv('WEATHER_API_KEY')
 REDDIT_API_KEY=os.getenv('REDDIT_API_KEY')
 
 bot = telebot.TeleBot(token=TOKEN)
+# bot = telebot.TeleBot(token=TOKEN, parse_mode="markdown")
 
 
 @bot.message_handler(commands=['roll'])
 def send(msg):
     bot.send_dice(chat_id=msg.chat.id)   
+
+
+def gen_markup():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 2
+
+    markup.add(
+        InlineKeyboardButton("Weather", callback_data="weather"),
+        InlineKeyboardButton("Memes", callback_data="memes"),
+        InlineKeyboardButton("Check out my source code", url="https://github.com/bharath1910/frankcrank" )
+        )
+
+    return markup
+
+def help_markup():
+    markup1 = InlineKeyboardMarkup()
+    markup1.row_width(2)
+
+    markup1.add(
+        InlineKeyboardButton("Contact", url="https://t.me/PythonNotFound"),
+        InlineKeyboardButton("Contact",url="https://t.me/shaunc276" ),
+        InlineKeyboardButton("Submit New Issue on GitHub", url="https://github.com/Bharath1910/frankcrank/issues")
+    )
+    
+    return markup1
 
 
 def get_weather(WEATHER_API_KEY, latitude, longitude):
@@ -32,8 +59,9 @@ def get_weather(WEATHER_API_KEY, latitude, longitude):
     feels_like = round((feels_like * 1.8)-459.67)
 
     humidity = result['main']['humidity']
-
+    weather = result['weather'][0]['main']
     final = {
+        "weather": weather,
         "temp": temperature,
         "feels_like": feels_like,
         "humidity": humidity
@@ -42,32 +70,39 @@ def get_weather(WEATHER_API_KEY, latitude, longitude):
 
 
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "How may I help you?\n1. /weather\n2. /memes")
+@bot.message_handler(commands=['start'])
+def message_handler(message):
+    bot.send_message(message.chat.id, "👋 *Hi, I am Frank.* \n \n🌤 I can send the current weather in your locality. \n🐸 I can send memes ;) \n\n🎲 Maybe throw a dice? - /roll \n\n*Developers*\n@PythonNotFound\n@shaunc276", reply_markup=gen_markup())
+
+@bot.message_handler(commands=['help'])
+def message_handler(msg):
+    bot.send_message(msg.chat.id, "Help", reply_markup=help_markup())
 
 
-@bot.message_handler(commands=['weather'])
-def send_welcome(message):
-	bot.reply_to(message, "Send a location to check weather.")
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == "weather":
+        bot.send_message(call.message.chat.id, "Send the location.")
 
+        @bot.message_handler(content_types=['location'])
+        def handle_location(message):
+            latitude = message.location.latitude
+            longitude = message.location.longitude
 
-
-@bot.message_handler(content_types=['location'])
-def handle_location(message):
-    latitude = message.location.latitude
-    longitude = message.location.longitude
-
-    weather_result=get_weather(WEATHER_API_KEY, latitude, longitude)
-    final = f"""
-                "temp": {weather_result['temp']},
-                "feels_like": {weather_result['feels_like']},
-                "humidity": {weather_result['humidity']}
-            """
-    bot.send_message(message.chat.id, final)
-
+            weather_result=get_weather(WEATHER_API_KEY, latitude, longitude)
     
-try:
-    bot.infinity_polling()
-except Exception:
-    time.sleep(15)
+            final = (
+                f"""
+                    weather: {weather_result['weather']}
+                    temperature: {weather_result['temp']},
+                    feels_like: {weather_result['feels_like']},
+                    humidity: {weather_result['humidity']}
+                """)
+            bot.send_message(message.chat.id, final)
+
+    elif call.data == "memes":
+        bot.send_message(call.message.chat.id, "Memes not added yet :P")
+        
+
+
+bot.infinity_polling()
