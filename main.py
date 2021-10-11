dae0193f61d756ba8239381101c7f12f03e8e51e
Bharath <av.bharathshanmugam@gmail.com>
2021-10-11 08:15:27 +0530

    changed something

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import telebot
 import time
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-token = "<API KEY>"
+token = "2062019393:AAFPC4C-O9QwNa1MSMowZVWKpTTeh9J2KQk"
 
 bot = telebot.TeleBot(token=token)
 
@@ -11,8 +11,8 @@ def gen_markup():
     markup.row_width = 2
 
     markup.add(
-        InlineKeyboardButton("Weather", callback_data="cb_yes"),
-        InlineKeyboardButton("Memes", callback_data="cb_no")
+        InlineKeyboardButton("Weather", callback_data="weather"),
+        InlineKeyboardButton("Memes", callback_data="memes")
         )
 
     return markup
@@ -21,24 +21,25 @@ def gen_markup():
 def message_handler(message):
     bot.send_message(message.chat.id, "Welcome!", reply_markup=gen_markup())
 
-
-
-
 @bot.message_handler(commands=['roll'])
 def send(msg):
     bot.send_dice(chat_id=msg.chat.id)
 
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
-    if call.data == "cb_yes":
-        bot.answer_callback_query(call.id, "Weather functions not added yet :P")
-    elif call.data == "cb_no":
-        bot.answer_callback_query(call.id, "Memes not added yet :P")
-
 @bot.message_handler(content_types=['location'])
 def handle_location(message):
     print("{0}, {1}".format(message.location.latitude, message.location.longitude))
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == "weather":
+        bot.send_message(call.message.chat.id, "Send the location.")
+
+        @bot.message_handler(content_types=['location'])
+        def handle_location(message):
+            print("{0}, {1}".format(message.location.latitude, message.location.longitude))
+
+    elif call.data == "memes":
+        bot.send_message(call.message.chat.id, "Memes not added yet :P")
 
 
 while True:
