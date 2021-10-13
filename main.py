216f186aa09298b3718e78e0ba225f9fb7177a5a
shaun276 <helloshaunak276@gmail.com>
2021-10-13 14:38:45 +0530

    added pomodoro and fun command

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 # dependencies
 import telebot
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+import praw
 from dotenv import load_dotenv
 import requests
 import math
 import os
+import time
+import random
+import sqlite3
+
 
 
 load_dotenv()
@@ -12,11 +17,31 @@ load_dotenv()
 # secrets
 TOKEN = os.getenv('TOKEN')
 WEATHER_API_KEY=os.getenv('WEATHER_API_KEY')
+CLIENT_ID=os.getenv('CLIENT_ID')
+CLIENT_SECRET=os.getenv('CLIENT_SECRET')
+PASSWORD=os.getenv('PASSWORD')
+USER_AGENT=os.getenv('USER_AGENT')
+USERNAME=os.getenv('USERNAME')
+
+# telebot setup
+bot = telebot.TeleBot(
+    token=TOKEN,
+    parse_mode="markdown"
+    )
 
 
-bot = telebot.TeleBot(token=TOKEN, parse_mode="markdown")
+# PRAW setup
+reddit = praw.Reddit(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    password=PASSWORD,
+    user_agent=USER_AGENT,
+    username=USERNAME
+)
 
- 
+
+conn = sqlite3.connect("db.db", check_same_thread=False)
+c = conn.cursor()
 
 
 def help_markup():
@@ -41,13 +66,31 @@ def gen_markup():
     """
 
     markup = InlineKeyboardMarkup()
-    markup.row_width = 2
+    markup.row_width = 3
 
     markup.add(
         InlineKeyboardButton("Weather", callback_data="weather"),
-        InlineKeyboardButton("Memes", callback_data="memes"),
+        InlineKeyboardButton("Fun", callback_data="fun"),
+        InlineKeyboardButton("Pomodoro Timer", callback_data="pomodoro"),
         InlineKeyboardButton("Check out my source code", url="https://github.com/bharath1910/frankcrank" )
         )
+
+    return markup
+
+def fun_markup():
+    """
+    Inline keyboard button layout, for fun
+    """
+
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 3
+
+    markup.add(
+        InlineKeyboardButton("Memes", callback_data="memes"),
+        InlineKeyboardButton("Aww", callback_data="aww"),
+        InlineKeyboardButton("Joke", callback_data="joke"),
+        InlineKeyboardButton("Shower Thoughts", callback_data="st")
+    )
 
     return markup
 
@@ -88,6 +131,56 @@ def get_weather(WEATHER_API_KEY, latitude, longitude):
     return final
 
 
+def st_reddit():
+    st = reddit.subreddit("Showerthoughts")
+    top = st.top(limit = 50)
+
+    submission = []
+
+    for i in top:
+        if not i.over_18:
+            submission.append(i)
+    
+    return random.choice(submission)
+
+def memes_reddit():
+    memes = reddit.subreddit("memes")
+    top = memes.top(limit=50)
+
+    submission = []
+
+    for i in top:
+        if not i.over_18:
+            submission.append(i)
+    
+    return random.choice(submission)
+
+def aww_reddit():
+    memes = reddit.subreddit("aww")
+    top = memes.top("week",limit=50)
+
+    submission = []
+
+    for i in top:
+        if not i.over_18:
+            if "jpg" in i.url[len(i.url) - 3:] or "png" in i.url[len(i.url)-3]:
+                submission.append(i)
+    
+    return random.choice(submission)
+
+    
+def jokes_reddit():
+    jokes = reddit.subreddit("Jokes")
+    top = jokes.top("week",limit=50)
+
+    submission = [] 
+    for i in top:
+        if not i.over_18:
+            submission.append(i)
+
+    return random.choice(submission)
+
+
 # /start
 @bot.message_handler(commands=['start'])
 def message_handler(message):
@@ -104,6 +197,28 @@ def msg_handler(msg):
 def send(msg):
     bot.send_dice(chat_id=msg.chat.id)  
 
+# /st, sends a shower thought
+@bot.message_handler(commands=['st'])
+def send(msg):
+    bot.send_message(msg.chat.id,st_reddit().title)
+
+# /meme, sends a meme
+@bot.message_handler(commands=['meme'])
+def send(msg):
+    memes = memes_reddit()
+    bot.send_photo(msg.chat.id, memes.url, memes.title)
+
+# /aww, sends an aww pic
+@bot.message_handler(commands=['aww'])
+def send(msg):
+    a = aww_reddit()
+    bot.send_photo(msg.chat.id, a.url, a.title)
+
+# /joke, sends a joke
+@bot.message_handler(commands=['joke'])
+def send(msg):
+    jokes = jokes_reddit()
+    bot.send_message(msg.chat.id,f"*{jokes.title}*\n\n{jokes.selftext}")
 
 
 def ms_km(num):
@@ -152,9 +267,93 @@ def callback_query(call):
 
             bot.send_message(message.chat.id,f"{weather_result['main']}, Expected {weather_result['description']} in {weather_result['city']}.\n\n*More Information:*\n    *-  Average Temperature* : {weather_result['temp']}C\n    *-  Minimum Temperature* : {weather_result['temp_min']}C\n    *-  Maximum Temperature* : {weather_result['temp_max']}C\n    *-  Atmospheric Pressure* : {weather_result['pressure']}hpa\n    *-  Humidity* : {weather_result['humidity']}%\n\nWind speed *{ms_km(weather_result['windspeed'])}km/h* {degrees_to_direction(weather_result['wind_degree'])}\n\n")
 
+    elif call.data == "pomodoro":
+        bot.send_message(call.message.chat.id, "start to start a timer, stop to cancel the timer")
 
+        @bot.message_handler(content_types=['text'])
+        def handle_pomodoro(message):
+            """
+            close, 1 = true, 0 = false
+            """
+
+            if message.text == "start":
+                result = c.execute("SELECT count FROM pomodoro WHERE user_id=?", (message.from_user.id,)).fetchone()
+                c.execute("UPDATE pomodoro SET close = ? WHERE user_id = ?", (0, message.from_user.id))
+                conn.commit()
+                if result is None:
+                    """
+                    if user_id is not present, it inserts user_id with count = 0 and close = 0
+                    """
+                    c.execute("INSERT INTO pomodoro VALUES(?, ?, ?)", (message.from_user.id, 0,0))
+                    conn.commit()
+                elif result is not None:
+                    """
+                    if user_id is present, it adds 1 to count
+                    """
+                    c.execute("UPDATE pomodoro SET count = count + 1 WHERE user_id=?", (message.from_user.id,))
+                    conn.commit()
+                
+                if int(result[0]) == 4:
+                    """
+                    if user is doing their fourth session, the break time will be 30 minutes
+                    """
+
+                    bot.send_message(call.message.chat.id, "Started a 25 minutes session.")
+                    time.sleep(1500)
+                    result = c.execute("SELECT close FROM pomodoro WHERE user_id=?", (message.from_user.id,)).fetchone()
+                    if result[0] == 0:
+                        bot.send_message(call.message.chat.id, "This was your 4th session, 30 minutes break started.")
+                    time.sleep(1800)
+                    result = c.execute("SELECT close FROM pomodoro WHERE user_id=?", (message.from_user.id,)).fetchone()
+                    if result[0] == 0:
+                        bot.send_message(call.message.chat.id, "30 minutes break ended, do you want to start a timer again? /start")
+                    
+                    """
+                    resets the user session count to 0
+                    """
+                    c.execute("UPDATE pomodoro SET count = ? WHERE user_id=?", (0, message.from_user.id))
+                    conn.commit()
+                    
+                else:
+                    bot.send_message(call.message.chat.id, "Started a 25 minutes session.")
+                    time.sleep(1500)
+                    result = c.execute("SELECT close FROM pomodoro WHERE user_id=?", (message.from_user.id,)).fetchone()
+                    if result[0] == 0:
+                        bot.send_message(call.message.chat.id, "5 minutes break started.")
+                    time.sleep(300)
+                    result = c.execute("SELECT close FROM pomodoro WHERE user_id=?", (message.from_user.id,)).fetchone()
+                    if result[0] == 0:
+                        bot.send_message(call.message.chat.id, "5 minutes break ended, do you want to start a timer again? /start")
+                    
+            elif message.text == "stop":
+                result = c.execute("SELECT close FROM pomodoro WHERE user_id=?", (message.from_user.id,)).fetchone()
+                if result is not None:
+                    """
+                    checks for the user_id, if present
+                    it sets close = 1
+                    """
+                    c.execute("UPDATE pomodoro SET close = ? WHERE user_id=?",(1, message.from_user.id))
+                    conn.commit()
+                    bot.send_message(call.message.chat.id, "Pomodoro timer stopped.")
+
+        
+    elif call.data == "fun":
+        bot.send_message(call.message.chat.id, "Hey!, So you finally decided to have some fun.\n\n*- Memes:* Sends top memes of the day [/meme]!\n*- Aww:* Get a cute and cuddly picture! [/aww]\n*- Jokes:* Sends tops jokes [/jokes]\n*- Shower Thoughts:* Get a shower thought [/st]\n\nNew features coming soon, stay in touch :D", reply_markup=fun_markup())
+
+    elif call.data == "st":
+        bot.send_message(call.message.chat.id, st_reddit().title)
+    
     elif call.data == "memes":
-        bot.send_message(call.message.chat.id, "Memes not added yet :P")
+        meme = memes_reddit()
+        bot.send_photo(call.message.chat.id,meme.url,meme.title)
+    
+    elif call.data == "aww":
+        a = aww_reddit()
+        bot.send_photo(call.message.chat.id, a.url,a.title)
+    
+    elif call.data == "joke":
+        jokes = jokes_reddit()
+        bot.send_message(call.message.chat.id,f"*{jokes.title}*\n\n{jokes.selftext}")
         
 
 # polling section
